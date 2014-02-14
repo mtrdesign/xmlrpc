347b69ed4add9372443015131c3c731f6b43e6fd
Михаил Милушев <lanzz@lanzz.org>
2014-02-14 02:32:57 +0200

    support for nulls in expected result of testcase since plists do not support nulls, nulls are encoded as strings with the magic value "!!null!!"

--- Unit Tests/XMLRPCParserTest.m
+++ Unit Tests/XMLRPCParserTest.m
@@ -34,14 +34,14 @@
 - (void)testEventBasedParser {
     NSEnumerator *testCaseEnumerator = [myTestCases keyEnumerator];
     id testCaseName;
-    
+
     while (testCaseName = [testCaseEnumerator nextObject]) {
         NSString *testCase = [[self unitTestBundle] pathForResource: testCaseName ofType: @"xml"];
         NSData *testCaseData =[[[NSData alloc] initWithContentsOfFile: testCase] autorelease];
         XMLRPCEventBasedParser *parser = [[[XMLRPCEventBasedParser alloc] initWithData: testCaseData] autorelease];
         id testCaseResult = [myTestCases objectForKey: testCaseName];
         id parsedResult = [parser parse];
-        
+
         STAssertTrue([self parsedResult: parsedResult isEqualToTestCaseResult: testCaseResult], @"The test case failed: %@", testCaseName);
     }
 }
@@ -67,7 +67,7 @@
 - (NSDictionary *)testCases {
     NSString *file = [[self unitTestBundle] pathForResource: @"TestCases" ofType: @"plist"];
     NSDictionary *testCases = [[[NSDictionary alloc] initWithContentsOfFile: file] autorelease];
-    
+
     return testCases;
 }
 
@@ -79,17 +79,21 @@
     } else if ([testCaseResult isKindOfClass: [NSDictionary class]]) {
         return [self parsedResult: parsedResult isEqualToDictionary: testCaseResult];
     }
-    
+
     if ([testCaseResult isKindOfClass: [NSNumber class]]) {
         return [parsedResult isEqualToNumber: testCaseResult];
     } else if ([testCaseResult isKindOfClass: [NSString class]]) {
-        return [parsedResult isEqualToString: testCaseResult];
+        if ([testCaseResult isEqualToString: @"!!null!!"]) {
+            return [parsedResult isEqual: [NSNull null]];
+        } else {
+            return [parsedResult isEqualToString: testCaseResult];
+        }
     } else if ([testCaseResult isKindOfClass: [NSDate class]]) {
         return [parsedResult isEqualToDate: testCaseResult];
     } else if ([testCaseResult isKindOfClass: [NSData class]]) {
         return [parsedResult isEqualToData: testCaseResult];
     }
-    
+
     return YES;
 }
 
@@ -98,52 +102,52 @@
 - (BOOL)parsedResult: (id)parsedResult isEqualToArray: (NSArray *)array {
     NSEnumerator *arrayEnumerator = [array objectEnumerator];
     id arrayElement;
-    
+
     if (![parsedResult isKindOfClass: [NSArray class]]) {
         return NO;
     }
-    
+
     if ([parsedResult count] != [array count]) {
         return NO;
     }
-    
+
     if ([parsedResult isEqualToArray: array]) {
         return YES;
     }
-    
+
     while (arrayElement = [arrayEnumerator nextObject]) {
         NSInteger index = [array indexOfObject: arrayElement];
-        
+
         if (![self parsedResult: [parsedResult objectAtIndex: index] isEqualToTestCaseResult: arrayElement]) {
             return NO;
         }
     }
-    
+
     return YES;
 }
 
 - (BOOL)parsedResult: (id)parsedResult isEqualToDictionary: (NSDictionary *)dictionary {
     NSEnumerator *keyEnumerator = [dictionary keyEnumerator];
     id key;
-    
+
     if (![parsedResult isKindOfClass: [NSDictionary class]]) {
         return NO;
     }
-    
+
     if ([parsedResult count] != [dictionary count]) {
         return NO;
     }
-    
+
     if ([parsedResult isEqualToDictionary: dictionary]) {
         return YES;
     }
-    
+
     while (key = [keyEnumerator nextObject]) {
         if (![self parsedResult: [parsedResult objectForKey: key] isEqualToTestCaseResult: [dictionary objectForKey: key]]) {
             return NO;
         }
     }
-    
+
     return YES;
 }
 
